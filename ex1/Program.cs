d7aff5ace030ee9ac04ed1d3650e0948056bd69c
Sofiya <sk074866336@gmail.com>
2023-09-28 20:13:53 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using System.Formats.Asn1;
+
+Random generator = new Random();
+
+int[] numbers = new int[generator.Next(0, 11)];
+for (int i = 0; i<numbers.Length; i++)
+{
+    numbers[i] = generator.Next(100, 1000);
+}
+int summ = 0;
+for (int j = 0; j<numbers.Length; j++)
+{
+    int a1 = numbers[j] % 10;
+    if (a1 % 2 == 0)
+    {
+        summ += 1;
+    }
+}
+Console.Write(summ);
